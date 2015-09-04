acb5d9b7cc454b830093d4786159eb65980f9912
Vyacheslav Gorishnyak <vgorishnyak@smithysoft.com>
2015-09-04 14:50:32 +0300

    добавил ауди на ховер + срипт для ауди !

--- for-portfolio/js/my-script.js
+++ for-portfolio/js/my-script.js
@@ -48,3 +48,22 @@ $(function() {
 		$('.none').toggleClass('hover')
 		  			});
     });
+
+/* script for audio*/
+
+$(function(){
+	$("#nav-two a")
+  .each(function(i) {
+    if (i != 0) { 
+      $("#beep-two")
+        .clone()
+        .attr("id", "beep-two" + i)
+        .appendTo($(this).parent()); 
+    }
+    $(this).data("beeper", i);
+  })
+  .mouseenter(function() {
+    $("#beep-two" + $(this).data("beeper"))[0].play();
+  });
+$("#beep-two").attr("id", "beep-two0");
+});
