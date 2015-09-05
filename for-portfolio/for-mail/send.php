11997c4e8ede6fda8fd7f9e1a5068d7da15f105e
sharnir <sharnirio@gmail.com>
2015-09-05 15:38:21 +0300

    создал возможность отправки сообщений !

---
+++ for-portfolio/for-mail/send.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style>
+   h1 { 
+    font-size: 120%; 
+    font-family: Verdana, Arial, Helvetica, sans-serif; 
+    color: #333366; 
+   }
+   .btn {
+   	background: #d16258;
+    color: #fff;
+    border: 2px solid #d16258;
+    padding: 13px 30px;
+    border-radius: 5px;
+   }
+  </style>
+</head>
+<body class="for-mail">
+
+<h1>Это статическая версия сайта, для возможности отправки сообщений перейдите по ссылке на полную версию сайта</h1>
+<a class="btn" href="http://my-resume.esy.es/">Ссылка</a>
+
+</body>
+</html>
+
+ 
