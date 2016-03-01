b126702202bd36def472226250efc159779921b3
sharnirio <sharnirio@mail.ru>
2016-03-01 14:49:48 +0200

    tz - 3

--- tz-3/js/my-js.js
+++ tz-3/js/my-js.js
@@ -4,21 +4,22 @@ function One()
   // Табы
   document.getElementById('OneTab').className = 'SelectedTab';
   document.getElementById('TwoTab').className = 'Tab';
- 
+
   // Страницы
   document.getElementById('One').style.display = 'block';
   document.getElementById('OneTab').className = 'SelectedTab';
   document.getElementById('Two').style.display = 'none';
- 
+
 }
 function Two()
 {
   // Табы
   document.getElementById('OneTab').className = 'Tab';
   document.getElementById('TwoTab').className = 'SelectedTab';
- 
+
   // Страницы
   document.getElementById('One').style.display = 'none';
   document.getElementById('Two').style.display = 'block';
- 
+
 }
+
