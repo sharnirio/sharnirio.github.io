686cf96120ae6ba045e131cb080b09c88c08b21b
sharnirio <sharnirio@mail.ru>
2016-03-08 18:16:35 +0200

    add new animation

--- for-portfolio/js/my-script.js
+++ for-portfolio/js/my-script.js
@@ -1,7 +1,8 @@
 /* script for scroll */
 
 $(function() {
-            $('a[href*=#]:not([href=#])').click(function() {
+	var elems = $("a[href*=#]:not([href=#])");
+            elems.click(function() {
               if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
