ffd319917ee86451b6cdc49e109b676f4c0ffe61
sharnirio <sharnirio@mail.ru>
2016-03-09 10:54:39 +0200

    add tz for js

---
+++ dz-js/tz.js
@@ -0,0 +1,49 @@
+ // comparison function
+ function compareAge(personA, personB) {
+  return personA.value < personB.value;
+}
+
+var people = [
+          {"name":"Alexey", "value": 74},
+          {"name":"Konstantin", "value": 93},
+          {"name":"Boris", "value": 23},
+          {"name":"Ivan", "value": 34},
+          {"name":"Anton", "value": 31},
+          {"name":"Volga", "value": 1},
+          {"name":"Samuil", "value": 19}
+ ];
+
+people.sort(compareAge);
+
+// withdraw
+for(var i = 0; i < people.length; i++) {
+	// Method 1 through alert
+  alert  ('{"name":' + '"' + people[i].name + '", ' + '"value":' + '"' + people[i].value + '"}'  );
+  // Method 2 through console
+  console.log(people[i])
+}
+
+// metod 3
+
+var desc, arr;
+
+desc = function (field) { // function to sort in reverse (descending)
+ return function (x, y) {
+   return x[field] < y[field];
+ }
+};
+
+arr = [
+          {"name":"Alexey", "value": 74},
+          {"name":"Konstantin", "value": 93},
+          {"name":"Boris", "value": 23},
+          {"name":"Ivan", "value": 34},
+          {"name":"Anton", "value": 31},
+          {"name":"Volga", "value": 1},
+          {"name":"Samuil", "value": 19}
+
+];
+
+console.log(arr.sort(desc('value')));
+
+
