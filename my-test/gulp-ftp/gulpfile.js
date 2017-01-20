c21f20bd90d6b75becb1800caf637fa4f7a8cd0b
sharnirio <sharnirio@gmail.com>
2017-01-20 15:14:55 +0200

    update my resume foto and update ftp gulp project!

--- my-test/gulp-ftp/gulpfile.js
+++ my-test/gulp-ftp/gulpfile.js
@@ -1,30 +1,33 @@
 var gulp = require( 'gulp' );
-var gutil = require( 'gulp-util' );
+// var gutil = require( 'gulp-util' );
 var ftp = require( 'vinyl-ftp' );
 
 gulp.task( 'deploy', function() {
 
     var conn = ftp.create( {
-        host:     '31.220.16.243',
-        user:     '****',
-        password: '****',
-        parallel: 10,
-        log:      gutil.log
+        host:     '178.63.197.105',
+        user:     'dvelop_test',
+        password: 'K6e2I1x4',
+        port: 21
     } );
 
     var globs = [
         // 'src/**',
-        'css/**',
+        'css/main2.css',
+        '!node_modules/**/*.css'
+        // 'style/css/main3.css',
         // 'js/**',
         // 'fonts/**',
-        'index.html'
+        // 'index2.html'
     ];
 
     // using base = '.' will transfer everything to /public_html correctly
     // turn off buffering in gulp.src for best performance
 
-    return gulp.src( globs, { base: '.', buffer: false } )
-        .pipe( conn.newer( '/public_html' ) ) // only upload newer files
-        .pipe( conn.dest( '/public_html' ) );
+    // return gulp.src( globs, { base: 'ohter', buffer: true } )
+    return gulp.src('style/css/main3.css')
+        .pipe( conn.newer( 'style/css/' ) ) // only upload newer files
+        .pipe( conn.dest( 'style2/test/3/' ) );
 
 } );
+
