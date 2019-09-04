3c0a50422bbc4dc4302b75e249258cb55b40b1df
sharnirio <sharnirio@gmail.com>
2019-09-04 11:03:23 +0300

    L01(firework) project - create structure

--- JS-learning/work-Oleg-dz-js/L01(firework)/markup-template/js/jquery.main.js
+++ JS-learning/work-Oleg-dz-js/L01(firework)/markup-template/js/jquery.main.js
@@ -1,32 +1,197 @@
-// To connect js scripts used plugin rigger included
-// Connection occurs through the design //= folder-name/file-name.js
-
+// function on
 jQuery(document).ready(function() {
-	jsActive("Js active");
+	greensockInit();
+	greensockInit2();
+	btmAnimate();
+	// timerInit();
 });
-
-
 //-------- -------- -------- --------
 //-------- js custom start
 //-------- -------- -------- --------
+let btmAnimate = () => {
+	let btnVar = document.querySelector('.btn-start');
+	let mainPageVar = document.querySelector('.main-section');
+	let jsHiddenVar = document.querySelectorAll('.js-hidden');
+	var tl = new TimelineMax();
+	btnVar.addEventListener('click', () => {
+		tl.to(mainPageVar, 1, { opacity: 0, scale: 0.9 }).set(jsHiddenVar, {className:"+=js-hidden-off"});
+	});
+}
 
-//function jsActive()
-function jsActive(arg) {
-	console.log(arg);
-};
+//https://www.youtube.com/watch?v=BAfiXPkubYw
+let timerInit = () => {
+	var time = 30,
+			fps = 60;
+	var Timer = function(obj) {
+		this.time = obj.time;
+		this.fps = obj.fps;
+		this.onEnd = obj.onEnd || null;
+		this.onStart = obj.onStart || null;
+		this.onTick = obj.onTick || null;
+		this.intervalID = null;
+		this.start = () => {
+			this.interval = setInterval(this.update, 1000 / this.fps);
+			this.onStart ? this.onStart() : void 0;
+			return this;
+		};
+		this.stop = () => {
+			clearInterval(this.interval);
+			this.onEnd ? this.onEnd() : void 0;
+		};
+		this.update = () => {
+			this.time > 0 ? this.time -= 1 / this.fps : this.stop();
+			this.onTick ? this.onTick() : void 0;
+			return this.get();
+		}
+		this.get = (par) => {
+			switch (par) {
+				case undefined:
+					return this.time;
+					break;
+				case "dig":
+					return Math.ceil(this.time);
+					break;
+				case "end":
+					return this.onEnd();
+					break;
+			}
+		}
+	}
+	var timer1 = new Timer({
+		time: time,
+		fps: fps,
+		onTick: tick,
+		onEnd: endTimer,
+		onStart: onTimerStart
+	});
 
+	function onTimerStart() {
+		console.log("timer started");
+	}
+
+	function endTimer() {
+		console.log("timer ended");
+	}
+	timer1.start()
+	requestAnimationFrame(tick);
+
+	function tick() {
+		id("output").innerHTML = timer1.get("dig");
+		id("slider").style.width = timer1.get() / time * 100 + "%";
+	}
+
+	function id(id) {
+		return document.getElementById(id);
+	}
+}
+// tutorial https://abraxabra.ru/blog/prochee/greensock-for-beginners-a-tutorial-on-web-animation-part-1/
+// function init
+function greensockInit() {
+	//first animation
+	var myElement = '.my-element';
+	TweenMax.set(myElement, { paddingBottom: 60, rotation: -15 }); // just css value
+	TweenMax.from(myElement, 6, { color: "#DC143C" });
+	TweenMax.to(myElement, 6, { rotation: 0, color: "#00FF00", delay: 6 });
+	//second animation with construction
+	var tl = new TimelineMax();
+	var myElement2 = '.my-element2';
+	var myElement3 = '.my-element3';
+	TweenMax.set(myElement2, { position: 'relative' });
+	tl.fromTo(myElement2, 4, {
+		// from state
+		opacity: 1,
+		top: "-100px",
+	}, {
+		// to end state
+		opacity: 0.5,
+		top: "100px",
+	}).to(myElement2, 4, { top: "-100px", opacity: 1, ease: Bounce.line }).to(myElement3, 4, { scale: 0.5, }).to(myElement2, 4, { top: "0px", scale: 0.8, })
+	//The third animation with group
+	var tl2 = new TimelineMax();
+	var myElement4 = '.my-element4';
+	var myElement4img = '.my-element4 img';
+	TweenMax.set(myElement4, { display: 'flex' });
+	tl2.staggerTo(myElement4img, 0.5, {
+		rotation: 180,
+	}, 1).staggerTo(myElement4img, 0.5, {
+		rotation: 0,
+	}, 1);
+}
+
+function greensockInit2() {
+	var tl = new TimelineMax();
+	var $box1 = $('.box1');
+	var $box2 = $('.box2');
+	var $box3 = $('.box3');
+	var $box4 = $('.box4');
+	var $allBox = $('.box1, .box2, .box3, .box4');
+	var restart = $('.restart-box');
+	var pause = $('.pause-box');
+	var play = $('.play-box');
+	tl.to($box1, 1, {
+		left: "100%",
+	}).to($box2, 1, {
+		right: "100%",
+	}, '-=1').to($box2, 1, {
+		bottom: "-100%",
+	}).to($box3, 1, {
+		top: "-100%",
+	}, '-=1').to($box4, 1, {
+		top: "-100%",
+	}).to($box1, 1, {
+		bottom: "-100%",
+	}, '-=1').to($box2, 1, {
+		right: "0",
+	}).to($box1, 1, {
+		left: "0",
+	}, '-=1').to($box1, 1, {
+		top: "0",
+	}).to($box2, 1, {
+		top: "0",
+	}, '-=1').to($box3, 1, {
+		top: "0%",
+	}, '-=1').to($box4, 1, {
+		top: "0%",
+	}, '-=1').to($allBox, 3, {
+		rotation: 180
+	}).to($allBox, 3, {
+		fontSize: "100px"
+	}, '+=1').add('newPoint').to($allBox, 5, {
+		scale: 0.5
+	}).to($allBox, 5, {
+		scale: 0.9
+	}).to($box1, 3, {
+		rotation: 360,
+		fontSize: "50px"
+	}, 'newPoint').to($box2, 3, {
+		rotation: 0,
+		fontSize: "80px"
+	}, 'newPoint').to($box3, 3, {
+		rotation: 360,
+		fontSize: "120px"
+	}, 'newPoint').to($box4, 3, {
+		rotation: 0,
+		fontSize: "140px"
+	}, 'newPoint');
+	restart.on('click', function() {
+		$allBox.removeAttr("style");
+		tl.restart();
+	})
+	pause.on('click', function() {
+		tl.pause();
+	})
+	play.on('click', function() {
+		tl.play();
+	})
+}
 //-------- -------- -------- --------
 //-------- js custom end
 //-------- -------- -------- --------
-
 //-------- -------- -------- --------
 //-------- included js libs start
 //-------- -------- -------- --------
-
-// just an example - how to connect library files such as slider, picturefill и other large js files
-// It is not necessary to work with js as before just keep in mind there is such an opportunity
-//= vendors/example-libs.js
-
+//-----module greensock js
+//= vendors/greensock.js
 //-------- -------- -------- --------
 //-------- included js libs end
 //-------- -------- -------- --------
--- JS-learning/work-Oleg-dz-js/L01(firework)/markup-template/js/vendors/example-libs.js
+++
@@ -1,8 +0,0 @@
-// To connect js scripts used plugin rigger included
-// Connection occurs through the design //= folder-name/file-name.js
-
-// just an example - how to connect library files such as slider, picturefill and other large js files
-// it is not necessary to work with js as before just keep in mind there is such an opportunity
-jQuery(document).ready(function() {
-	console.log("I'm just an example");
-});
