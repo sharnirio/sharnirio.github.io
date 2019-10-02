d7064d11bc3c79b1a16bd355989a93952c04c2f5
sharnirio <sharnirio@gmail.com>
2019-10-02 13:57:48 +0300

    L02(subway)-V2 project - change library for drag and drop functionality

--- JS-learning/work-Oleg-dz-js/L02(subway)-V2/markup-template/js/jquery.main.js
+++ JS-learning/work-Oleg-dz-js/L02(subway)-V2/markup-template/js/jquery.main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-clickForStaf(staffClassList);
-pageReload();
+	displaceInit(staffClassList);
+	pageReload();
 }, false);
 //-------- -------- -------- --------
 //-------- js custom start
@@ -9,34 +9,72 @@ pageReload();
 // tutorial https://abraxabra.ru/blog/prochee/greensock-for-beginners-a-tutorial-on-web-animation-part-1/
 var staffClassList = [".salat", ".onion", ".chicken", ".tomato", ".cucumber", ".cheese", ".chips", ".bob", ".becon"]
 
-function clickForStaf(arr) {
-	let classList = arr;
-	let classListLength = classList.length;
-	let fergit = $(".fergit");
-	let shayrma = $(".shayrma");
-	let burger = $(".burger");
-	let blockBtn = $(".block-btn img");
-	var i2 = 0;
-	for (let i = 0; i < classListLength; i++) {
-		let $class = $(`.block-item${classList[i]}`);
+function displaceInit(arg) {
 
-		function clickFun() {
-			$class.on('click', function() {
-				let tl = new TimelineMax();
-				let classImg = $(this).find("img");
-				let classImgPos = classImg.position();
-				let $classStaff = $(`.block-staffing ${classList[i]}`);
-				i2++;
-				tl.set(classImg, { position: "absolute", top: classImgPos.top, left: classImgPos.left }).to(classImg, 1.2, { left: "50%", top: "50%", opacity: 0.4, x: "-50%", y: "-50%" }).to(classImg, 0.3, { opacity: 0 }).to($classStaff, 0.5, { scale: 1, opacity: 1 });
-				if (i2 == classListLength) {
-					let tl2 = new TimelineMax();
-					tl2.add('point').to(blockBtn, 2, { opacity: 0, x: "-100%" }, 'point').to(fergit, 1.2, { scale: 1, opacity: 1, x: "-50%" }, 'point').add('newPoint').to(fergit, 1, { scale: 0, opacity: 0 }, 'newPoint+=6').to(burger, 1, { scale: 0, opacity: 0 }, 'newPoint+=6').to(shayrma, 1, { scale: 1, opacity: 1, x: "-50%" })
-				}
-			});
-		}
-		clickFun();
+	function dragMoveListener(event) {
+		var target = event.target
+		// keep the dragged position in the data-x/data-y attributes
+		var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+		var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+		// translate the element
+		target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+		// update the posiion attributes
+		target.setAttribute('data-x', x)
+		target.setAttribute('data-y', y)
 	}
+	// this is used later in the resizing and gesture demos
+	window.dragMoveListener = dragMoveListener
+
+	/* The dragging code for '.draggable' from the demo above
+	 * applies to this demo as well so it doesn't have to be repeated. */
+	// enable draggables to be dropped into this
+	interact('.block-staffing').dropzone({
+		// only accept elements matching this CSS selector
+		accept: '.block-item:not(.block-btn):not(.burger)',
+		// Require a 75% element overlap for a drop to be possible
+		overlap: 0.1,
+		// listen for drop related events:
+		ondropactivate: function(event) {
+			// add active dropzone feedback
+			event.target.classList.add('drop-active')
+		},
+		ondragenter: function(event) {
+			var draggableElement = event.relatedTarget
+			var dropzoneElement = event.target
+			// feedback the possibility of a drop
+			dropzoneElement.classList.add('drop-target')
+			draggableElement.classList.add('can-drop')
+		},
+		ondragleave: function(event) {
+			// remove the drop feedback style
+			event.target.classList.remove('drop-target')
+			event.relatedTarget.classList.remove('can-drop')
+		},
+		ondrop: function(event) {
+			console.log('Dropped');
+			// event.relatedTarget.textContent = 'Dropped'
+		},
+		ondropdeactivate: function(event) {
+			// remove active dropzone feedback
+			event.target.classList.remove('drop-active')
+			event.target.classList.remove('drop-target')
+		}
+	})
+
+	interact('.block-inner > .block-item:not(.block-btn):not(.burger)').draggable({
+		inertia: true,
+		modifiers: [
+			interact.modifiers.restrictRect({
+				restriction: 'parent',
+				endOnly: true
+			})
+		],
+		autoScroll: true,
+		// dragMoveListener from the dragging demo above
+		onmove: dragMoveListener
+	})
 }
+
 
 let pageReload = () => {
 	let btnVar = document.querySelector('.shayrma a');
@@ -52,8 +90,8 @@ let pageReload = () => {
 //-------- included js libs start
 //-------- -------- -------- --------
 
-
 //= vendors/greensock.js
+//= vendors/displace.min.js
 
 //-------- -------- -------- --------
 //-------- included js libs end
