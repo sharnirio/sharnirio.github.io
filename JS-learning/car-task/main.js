018849413e057b6e69f44d5f3849b155c3341946
sharnirio <sharnirio@gmail.com>
2019-12-15 19:39:50 +0200

    CSS task - project - create structure page

---
+++ JS-learning/car-task/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function () {
+	jsActive("Js active");
+});
+
+
+//-------- -------- -------- --------
+//-------- js custom start
+//-------- -------- -------- --------
+
+//function jsActive()
+function jsActive(arg) {
+	console.log(arg);
+};
