0f3b07f0c7d8c5a84033d27b4a00d664b8ff9255
sharnirio <sharnirio@gmail.com>
2019-12-17 21:31:59 +0200

    CSS task - project - create cloud animation

--- JS-learning/car-task/main.js
+++ JS-learning/car-task/main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
-	jsActive("Js active");
+	cloudA();
 });
 
 
@@ -8,6 +8,23 @@ jQuery(document).ready(function () {
 //-------- -------- -------- --------
 
 //function jsActive()
-function jsActive(arg) {
-	console.log(arg);
-};
+function cloudA() {
+	// tlTop.staggerTo(topImg, 3, {left:"+100%"}, 0);
+
+	var anLeft = $('.an-l');
+	var anRight = $('.an-r');
+	var anSun = $('.an-s');
+	var anDuration = 9;
+	// var tlAnLeft = gsap.timeline({repeat: -1, repeatDelay: 0, ease: "none"});
+
+	gsap.to(anLeft, {repeat: -1, ease: "none",duration: anDuration, stagger: 3, css: {left:"+100%"}});
+	gsap.to(anLeft, {repeat: -1, ease: "none", yoyo:true,duration: 2, css: {y:"70%"}});
+
+	gsap.to(anRight, {delay: -anDuration-1, repeat: -1, ease: "none",duration: anDuration, stagger: 3, css: {right:"+100%"}});
+	gsap.to(anRight, {repeat: -1, ease: "none", yoyo:true,duration: 2, css: {y:"-70%"}});
+
+	gsap.to(anSun, {repeat: -1, ease: "none", yoyo:true, duration: 10, css: {right:"+50%",
+	bottom: "-90vh"}});
+
+
+}
